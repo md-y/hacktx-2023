0080039048bdf71a0f9eea2312057959780eab6a
jayeshpaluru <113400123+jayeshpaluru@users.noreply.github.com>
2023-10-21 18:54:33 -0500

    Update +page.svelte
    Added code that was translated from Functions.py

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -70,6 +70,97 @@
 			console.log(error);
 		}
 	}
+	const math = require('mathjs');
+	const fetch = require('node-fetch'); 
+
+	function stringToFloat(s) {
+  		return parseFloat(s.replace(',', ''));
+	}
+
+	function calculateLinearR(initial_value, buy_date, current_value, current_date) {
+  		return (current_value - initial_value) / (current_date - buy_date);
+	}
+
+	function calculateExponentialR(initial_value, buy_date, current_value, current_date) {
+  	if (current_date === buy_date) {
+      	return 0;
+  	}
+  	return (current_value / initial_value) ** (1 / (current_date - buy_date)) - 1;
+	}
+
+	// Newton-Raphson Method
+	function newtonRaphson(func, derivative, initialGuess, tolerance = 1e-6, maxIterations = 1000) {
+  		let x0 = initialGuess;
+  		let x1;
+
+  		for (let i = 0; i < maxIterations; i++) {
+      		x1 = x0 - func(x0) / derivative(x0);
+
+      		if (Math.abs(x1 - x0) <= tolerance) {
+          		return x1;
+      		}
+
+      		x0 = x1;
+  		}	
+  		throw new Error('Newton-Raphson method did not converge');
+	}
+
+	function calculateLogisticR(initial_value, buy_date, current_value, current_date) {
+  		const L = 2 * initial_value;
+  		const time_difference = current_date - buy_date;
+  		const c = L / initial_value - 1;
+
+  		const logisticFunction = (R) => {
+      		return current_value - (L / (1 + c * Math.exp(-R * time_difference)));
+  		};
+
+  		const logisticFunctionDerivative = (R) => {
+      		return L * c * time_difference * Math.exp(-R * time_difference) / 
+             ((1 + c * Math.exp(-R * time_difference)) ** 2);
+  		};
+
+  		try {
+      		const R = newtonRaphson(logisticFunction, logisticFunctionDerivative, 0.05);
+      		return R;
+  		} catch (e) {
+      		console.error(e.message);
+      		return null;
+  		}
+	}
+
+	async function suggestRValue(asset, function_type) {
+    const API_ENDPOINT = "https://api.openai.com/v1/engines/davinci/completions"; 
+    const API_KEY = "YOUR_OPENAI_API_KEY";
+
+    const messages = [{
+        "role": "system",
+        "content": `Please provide a ${function_type} growth/decay rate, R, for modeling ${asset} appreciation/depreciation. Make sure it is realistic and varies with different assets and function types. Make the R value negative if you think the asset depreciates and positive if you think the asset appreciates. Always return an R value.`
+    }];
+
+    const response = await fetch(API_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: messages,
+            max_tokens: 2000,
+        })
+    });
+
+    const data = await response.json();
+
+    const suggested_R_value = data.choices[0].message.content.trim();
+    const numberMatches = suggested_R_value.match(/[-+]?\d*\.\d+|\d+/g);
+
+    if (!numberMatches) {
+        throw new Error("Failed to extract a valid number from GPT response.");
+    }
+
+    return parseFloat(numberMatches[0]);
+	}
 </script>
 
 <button on:click={login}>Login</button>
