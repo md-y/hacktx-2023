005105d6806ea949b28838ecece9ccfd32424eb1
md-y <15069105+md-y@users.noreply.github.com>
2023-10-21 20:32:37 -0500

    Add basic modal form

---
+++ src/lib/components/NewAssetModal.svelte
@@ -0,0 +1,21 @@
+<script>
+	// @ts-nocheck
+
+	import { Modal, Form, TextInput } from 'carbon-components-svelte';
+	export let open;
+</script>
+
+<Modal
+	bind:open
+	modalHeading="Create database"
+	primaryButtonText="Confirm"
+	secondaryButtonText="Cancel"
+	on:click:button--secondary={() => (open = false)}
+	on:open
+	on:close
+	on:submit
+>
+	<Form>
+		<TextInput id="asset-name" />
+	</Form>
+</Modal>
