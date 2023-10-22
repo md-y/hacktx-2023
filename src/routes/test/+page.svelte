0ecc62004707cfe53157a031df8a2a789ee2d26e
Nevin Gilday <nevingilday@gmail.com>
2023-10-22 04:37:11 -0500

    allows new login users to create a profile... I think

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,12 +1,12 @@
 <script>
 	// @ts-nocheck
 
-		// Import the functions you need from the SDKs you need
-	import { initializeApp } from "firebase/app";
-	import { getAuth, signInWithPopup, signOut} from "firebase/auth";
-	import { GoogleAuthProvider } from "firebase/auth";
+	// Import the functions you need from the SDKs you need
+	import { initializeApp } from 'firebase/app';
+	import { getAuth, signInWithPopup, signOut } from 'firebase/auth';
+	import { GoogleAuthProvider } from 'firebase/auth';
 	import AES from 'crypto-js/aes';
-	import CryptoJS from 'crypto-js'
+	import CryptoJS from 'crypto-js';
 
 	// TODO: Add SDKs for Firebase products that you want to use
 	// https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,16 +14,14 @@
 	// Your web app's Firebase configuration
 	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 	const firebaseConfig = {
-	apiKey: "AIzaSyBWqnf7hw8AgFif5swkvWJx3CoERDJCgdo",
-	authDomain: "hacktx2023.firebaseapp.com",
-	projectId: "hacktx2023",
-	storageBucket: "hacktx2023.appspot.com",
-	messagingSenderId: "908408891172",
-	appId: "1:908408891172:web:467345a36027012a1e75b1",
-	measurementId: "G-SS4CK9CSEH"
+		apiKey: 'AIzaSyBWqnf7hw8AgFif5swkvWJx3CoERDJCgdo',
+		authDomain: 'hacktx2023.firebaseapp.com',
+		projectId: 'hacktx2023',
+		storageBucket: 'hacktx2023.appspot.com',
+		messagingSenderId: '908408891172',
+		appId: '1:908408891172:web:467345a36027012a1e75b1',
+		measurementId: 'G-SS4CK9CSEH'
 	};
-
-
 
 	// Initialize Firebase
 	const app = initializeApp(firebaseConfig);
@@ -37,92 +35,114 @@
 	let uid = undefined;
 	let decryptPassword = undefined;
 
-	auth.onAuthStateChanged(user => {
+	auth.onAuthStateChanged((user) => {
 		loggedIn = user ? true : false;
-		if(loggedIn){
+		if (loggedIn) {
 			uid = user.uid;
-			if(authToken == undefined){
+			if (authToken == undefined) {
 				fetchAuthToken();
 			}
-		}else{
+		} else {
 			uid = undefined;
 			authToken = undefined;
 		}
-	})
+	});
 
-	async function fetchAuthToken(){
+	async function fetchAuthToken() {
 		if (auth.currentUser) {
 			authToken = await auth.currentUser.getIdToken(true);
 			authToken = authToken;
 		}
 	}
 
-	function login(){
-		signInWithPopup(auth, provider);
-		fetchAuthToken();
-		console.log("logging you in");
-		console.log(auth)
+	function login() {
+		try {
+			signInWithPopup(auth, provider);
+			fetchAuthToken();
+			console.log('logging you in');
+			console.log(auth);
+		} catch (error) {
+			userData = {
+				assets: [
+				],
+				bills: [
+				],
+				checking_account_balance: 0,
+				checking_account_reward: 0,
+				credit_card_balance: 0,
+				credit_card_rewards: 0,
+				deposits: [
+				],
+				monthly_bills: [
+				],
+				savings_account_balance: 0,
+				savings_account_rewards: 0,
+				withdrawls: [
+				]
+			};
+			postData();
+		}
 	}
 
-	function logout(){
+	function logout() {
 		signOut(auth);
-		console.log("logging you out");
+		console.log('logging you out');
 	}
 
-	async function getData(){
-		try{
-			let response = await fetch("https://helloworld-feagyby2hq-uc.a.run.app/user", {
+	async function getData() {
+		try {
+			let response = await fetch('https://helloworld-feagyby2hq-uc.a.run.app/user', {
 				headers: {
-					"AuthToken": authToken
+					AuthToken: authToken
 				}
 			});
 			userData = await response.json();
-		}
-		catch(error){
-			console.log(error)
+		} catch (error) {
+			console.log(error);
 		}
 	}
 
-	async function postData(){
-		try{
-			let response = await fetch("https://helloworld-feagyby2hq-uc.a.run.app/user", {
-				method: "POST",
+	async function postData() {
+		try {
+			let response = await fetch('https://helloworld-feagyby2hq-uc.a.run.app/user', {
+				method: 'POST',
 				headers: {
-					"Content-Type": "application/json",
-					"AuthToken": authToken
+					'Content-Type': 'application/json',
+					AuthToken: authToken
 				},
 				body: JSON.stringify(userData)
 			});
-		}
-		catch(error){
-			console.log(error)
+		} catch (error) {
+			console.log(error);
 		}
 	}
 
-	async function pushData(){
-		try{
-			let response = await fetch("https://helloworld-feagyby2hq-uc.a.run.app/user", {
-				method: "PUT",
+	async function pushData() {
+		try {
+			let response = await fetch('https://helloworld-feagyby2hq-uc.a.run.app/user', {
+				method: 'PUT',
 				headers: {
-					"Content-Type": "application/json",
-					"AuthToken": authToken
+					'Content-Type': 'application/json',
+					AuthToken: authToken
 				},
 				body: JSON.stringify(userData)
 			});
-		}
-		catch(error){
-			console.log(error)
+		} catch (error) {
+			console.log(error);
 		}
 	}
 
-	function downloadData(){
+	function downloadData() {
 		//code to encrypt file
 		let encryptedData = encryptData(JSON.stringify(userData), decryptPassword).toString();
 
 		//code to download file
 		var element = document.createElement('a');
-		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(encryptedData));
-		element.setAttribute('download', "my_secure_financial_data.txt");
+		element.setAttribute(
+			'href',
+			'data:text/plain;charset=utf-8,' + encodeURIComponent(encryptedData)
+		);
+		element.setAttribute('download', 'my_secure_financial_data.txt');
 
 		element.style.display = 'none';
 		document.body.appendChild(element);
@@ -133,17 +153,19 @@
 	}
 
 	async function previewFile() {
-		const content = document.querySelector(".content");
-		const [file] = document.querySelector("input[type=file]").files;
+		const content = document.querySelector('.content');
+		const [file] = document.querySelector('input[type=file]').files;
 		const reader = new FileReader();
 
 		reader.addEventListener(
-			"load",
+			'load',
 			() => {
-				//this updates the userData 
-				userData = JSON.parse(decryptData(reader.result, decryptPassword).toString(CryptoJS.enc.Utf8));
+				//this updates the userData
+				userData = JSON.parse(
+					decryptData(reader.result, decryptPassword).toString(CryptoJS.enc.Utf8)
+				);
 			},
-			false,
+			false
 		);
 
 		if (file) {
@@ -152,99 +174,103 @@
 	}
 
 	function stringToFloat(s) {
-  		return parseFloat(s.replace(',', ''));
+		return parseFloat(s.replace(',', ''));
 	}
 
 	function calculateLinearR(initial_value, buy_date, current_value, current_date) {
-  		return (current_value - initial_value) / (current_date - buy_date);
+		return (current_value - initial_value) / (current_date - buy_date);
 	}
 
 	function calculateExponentialR(initial_value, buy_date, current_value, current_date) {
-  	if (current_date === buy_date) {
-      	return 0;
-  	}
-  	return (current_value / initial_value) ** (1 / (current_date - buy_date)) - 1;
+		if (current_date === buy_date) {
+			return 0;
+		}
+		return (current_value / initial_value) ** (1 / (current_date - buy_date)) - 1;
 	}
 
 	// Newton-Raphson Method
 	function newtonRaphson(func, derivative, initialGuess, tolerance = 1e-6, maxIterations = 1000) {
-  		let x0 = initialGuess;
-  		let x1;
+		let x0 = initialGuess;
+		let x1;
 
-  		for (let i = 0; i < maxIterations; i++) {
-      		x1 = x0 - func(x0) / derivative(x0);
+		for (let i = 0; i < maxIterations; i++) {
+			x1 = x0 - func(x0) / derivative(x0);
 
-      		if (Math.abs(x1 - x0) <= tolerance) {
-          		return x1;
-      		}
+			if (Math.abs(x1 - x0) <= tolerance) {
+				return x1;
+			}
 
-      		x0 = x1;
-  		}	
-  		throw new Error('Newton-Raphson method did not converge');
+			x0 = x1;
+		}
+		throw new Error('Newton-Raphson method did not converge');
 	}
 
 	function calculateLogisticR(initial_value, buy_date, current_value, current_date) {
-  		const L = 2 * initial_value;
-  		const time_difference = current_date - buy_date;
-  		const c = L / initial_value - 1;
+		const L = 2 * initial_value;
+		const time_difference = current_date - buy_date;
+		const c = L / initial_value - 1;
 
-  		const logisticFunction = (R) => {
-      		return current_value - (L / (1 + c * Math.exp(-R * time_difference)));
-  		};
+		const logisticFunction = (R) => {
+			return current_value - L / (1 + c * Math.exp(-R * time_difference));
+		};
 
-  		const logisticFunctionDerivative = (R) => {
-      		return L * c * time_difference * Math.exp(-R * time_difference) / 
-             ((1 + c * Math.exp(-R * time_difference)) ** 2);
-  		};
+		const logisticFunctionDerivative = (R) => {
+			return (
+				(L * c * time_difference * Math.exp(-R * time_difference)) /
+				(1 + c * Math.exp(-R * time_difference)) ** 2
+			);
+		};
 
-  		try {
-      		const R = newtonRaphson(logisticFunction, logisticFunctionDerivative, 0.05);
-      		return R;
-  		} catch (e) {
-      		console.error(e.message);
-      		return null;
-  		}
+		try {
+			const R = newtonRaphson(logisticFunction, logisticFunctionDerivative, 0.05);
+			return R;
+		} catch (e) {
+			console.error(e.message);
+			return null;
+		}
 	}
 
 	async function suggestRValue(asset, function_type) {
-    const API_ENDPOINT = "https://api.openai.com/v1/engines/davinci/completions"; 
-    const API_KEY = "YOUR_OPENAI_API_KEY";
+		const API_ENDPOINT = 'https://api.openai.com/v1/engines/davinci/completions';
+		const API_KEY = 'YOUR_OPENAI_API_KEY';
 
-    const messages = [{
-        "role": "system",
-        "content": `Please provide a ${function_type} growth/decay rate, R, for modeling ${asset} appreciation/depreciation. Make sure it is realistic and varies with different assets and function types. Make the R value negative if you think the asset depreciates and positive if you think the asset appreciates. Always return an R value.`
-    }];
+		const messages = [
+			{
+				role: 'system',
+				content: `Please provide a ${function_type} growth/decay rate, R, for modeling ${asset} appreciation/depreciation. Make sure it is realistic and varies with different assets and function types. Make the R value negative if you think the asset depreciates and positive if you think the asset appreciates. Always return an R value.`
+			}
+		];
 
-    const response = await fetch(API_ENDPOINT, {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${API_KEY}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: messages,
-            max_tokens: 2000,
-        })
-    });
+		const response = await fetch(API_ENDPOINT, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${API_KEY}`,
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				model: 'gpt-3.5-turbo',
+				messages: messages,
+				max_tokens: 2000
+			})
+		});
 
-    const data = await response.json();
+		const data = await response.json();
 
-    const suggested_R_value = data.choices[0].message.content.trim();
-    const numberMatches = suggested_R_value.match(/[-+]?\d*\.\d+|\d+/g);
+		const suggested_R_value = data.choices[0].message.content.trim();
+		const numberMatches = suggested_R_value.match(/[-+]?\d*\.\d+|\d+/g);
 
-    if (!numberMatches) {
-        throw new Error("Failed to extract a valid number from GPT response.");
-    }
+		if (!numberMatches) {
+			throw new Error('Failed to extract a valid number from GPT response.');
+		}
 
-    return parseFloat(numberMatches[0]);
+		return parseFloat(numberMatches[0]);
 	}
 
-	function encryptData(data, key){
+	function encryptData(data, key) {
 		return AES.encrypt(data, key);
 	}
 
-	function decryptData(data, key){
+	function decryptData(data, key) {
 		return AES.decrypt(data, key);
 	}
 </script>
@@ -256,8 +282,8 @@
 <button on:click={postData}>Post data</button>
 <button on:click={pushData}>Put data</button>
 <button on:click={downloadData}>Download data</button>
-<input type="file" on:change={previewFile}/>
+<input type="file" on:change={previewFile} />
 <label for="decryptPassword"> password for file</label>
-<input id="decryptPassword" type="password" bind:value={decryptPassword}/>
-<p class="content"></p>
+<input id="decryptPassword" type="password" bind:value={decryptPassword} />
+<p class="content" />
 <p>{JSON.stringify(userData)}</p>
